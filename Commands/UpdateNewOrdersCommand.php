988f65b39f7d06d979a3e3e0f1d6ac246ad048b4
MichelAngelo <baksdevelopmen@gmail.com>
2026-01-15 17:58:50 +0300

    BaksDevelopment

--- Commands/UpdateNewOrdersCommand.php
+++ Commands/UpdateNewOrdersCommand.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -31,8 +31,6 @@ use BaksDev\Megamarket\Orders\Messenger\NewOrders\NewMegamarketOrderMessage;
 use BaksDev\Megamarket\Repository\AllProfileToken\AllProfileMegamarketTokenInterface;
 use BaksDev\Orders\Order\Repository\ExistsOrderNumber\ExistsOrderNumberInterface;
 use BaksDev\Users\Profile\UserProfile\Type\Id\UserProfileUid;
-use BaksDev\Yandex\Market\Orders\Api\megamarketNewOrdersRequest;
-use BaksDev\Yandex\Market\Orders\UseCase\New\YandexMarketOrderHandler;
 use DateInterval;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
